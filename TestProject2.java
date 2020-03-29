f4fb13ca104aac97641c80bc5a18e926a40ea6d7
iamemderoy <ethanderoy@gmail.com>
2020-03-29 16:31:32 -0500

    test Java file

---
+++ TestProject2.java
@@ -0,0 +1,23 @@
+package testproject2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Ethan DeRoy
+ */
+public class TestProject2 
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please Enter your Net-ID: ");
+        String netID = in.next();
+        System.out.println(netID);
+    }
+    
+}
